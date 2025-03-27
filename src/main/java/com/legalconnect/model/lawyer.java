547320009a54de1e130243145d147a7d684630e4
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-27 21:24:30 +0530

    creating lawyer profile

---
+++ src/main/java/com/legalconnect/model/lawyer.java
@@ -0,0 +1,8 @@
+package com.legalconnect.model;
+
+import lombok.Data;
+
+@Data
+public class lawyer {
+
+}
---
+++ src/main/java/com/legalconnect/utility/Data.java
@@ -0,0 +1,9 @@
+package com.legalconnect.utility;
+
+import java.util.ArrayList;
+
+public class Data {
+
+  ArrayList<Integer> lawyer = new ArrayList<>();
+
+}
